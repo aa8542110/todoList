51e07f042f956c7a28588d337f41ec0accbd15f9
DESKTOP-D2HIFNV\aa854 <aa8542110@gmail.com>
2018-06-13 20:40:06 +0800

    Done

--- backend/src/App.vue
+++ backend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="body">
     <div class="d-flex justify-content-center header">
       <div class="col text-center py-3" v-bind:class="{ active: active ==1 }"
        v-on:click="change(1)">My Tasks</div>
@@ -8,34 +8,96 @@
       <div class="col text-center py-3" v-bind:class="{ active: active ==3 }"
        v-on:click="change(3)">Completed</div>
     </div>
-    <div v-for="(item,index) in Task" class="card mt-3 mx-2 px-5 py-2" :key="index">
-      <div class=" mt-2  px-5 py-2">
-        <div class="form-group">
-          <h5>Deadline</h5>
-          <div class="block">
-            <el-date-picker v-model="date" type="date" placeholder="選擇日期">
-            </el-date-picker>
-          </div>
-          <h5>File</h5>
-          <label for="input-file">
-            <div class="bg-gray">
-            <i class="el-icon-plus"></i>
+    <div class="addcontent">
+      <div class="card  mx-auto">
+        <div class="addView" v-if="additem.show">
+          <div class="viewHead">
+            <div class="bottomLine">
+              <input type="text" class="title-text mt-2  mx-5 py-2"
+               v-model="additem.title" placeholder="Put your title">
             </div>
-            <input id="input-file"   type="file">
-          </label>
-
-          <h5>Comment</h5>
-          <input class="form-control" />
+          </div>
+          <div class=" mt-2  mx-5 py-2">
+            <div class="form-group">
+              <h5>Deadline</h5>
+              <div class="block">
+                <el-date-picker v-model="additem.date" type="date" placeholder="選擇日期">
+                </el-date-picker>
+              </div>
+              <h5>File</h5>
+              <label for="input-file">
+                <span>{{additem.file}}</span>
+                <div class="bg-gray">
+                  <i class="el-icon-plus"></i>
+                </div>
+                <input id="input-file" type="file">
+              </label>
+              <h5>Comment</h5>
+              <input class="form-control" />
+            </div>
+            <div class="btn-group d-flex justify-content-center"
+             role="group" aria-label="Basic example">
+              <el-button-group>
+                <el-button type="primary" @click="showAddView(false)">cancel</el-button>
+                <el-button type="primary" @click="addTask(additem)">Submit</el-button>
+              </el-button-group>
+            </div>
+          </div>
         </div>
-
-      </div>
-      <div class="btn-group d-flex justify-content-center" role="group" aria-label="Basic example">
-        <el-button-group>
-          <el-button type="primary">cancel</el-button>
-          <el-button type="primary">Submit</el-button>
-        </el-button-group>
+        <div class="addbox p-3" v-else @click="showAddView(true)">
+          <i class="el-icon-plus"></i>
+          <span class="addbox-font">Add Content</span>
+        </div>
       </div>
     </div>
+    <div class="content">
+      <div v-for="(item,index) in Task" class=" mx-auto" :key="index">
+        <div class="listView card" v-if="item.show">
+          <div class="viewHead">
+            <div class="bottomLine">
+              <input type="text" class="title-text mt-2  mx-5 py-2"
+               v-model="item.title" placeholder="Put your title">
+
+            </div>
+          </div>
+          <div class=" mt-2  mx-5 py-2">
+            <div class="form-group">
+              <h5>Deadline</h5>
+              <div class="block">
+                <el-date-picker v-model="item.date" type="date" placeholder="選擇日期">
+                </el-date-picker>
+              </div>
+              <h5>File</h5>
+              <label for="input-file">
+                <span>{{item.file}}</span>
+                <div class="bg-gray">
+                  <i class="el-icon-plus"></i>
+                </div>
+                <input id="input-file" type="file">
+              </label>
+              <h5>Comment</h5>
+              <input class="form-control" />
+            </div>
+            <div class="btn-group d-flex justify-content-center"
+             role="group" aria-label="Basic example">
+              <el-button-group>
+                <el-button type="primary" @click="showAddView(false)">cancel</el-button>
+                <el-button type="primary">Submit</el-button>
+              </el-button-group>
+            </div>
+          </div>
+
+        </div>
+        <div v-else class="View my-3 py-3">
+        <div class="ml-3">{{item.title}}<i class="el-icon-edit-outline float-right mr-2"
+        @click="editView(index)"></i></div>
+        <div class="ml-3"><i class="el-icon-time  mr-2" />{{item.date}} </div>
+
+        </div>
+      </div>
+
+    </div>
+
   </div>
 
 </template>
@@ -45,38 +107,54 @@ export default {
   data() {
     return {
       Task: [{
-        Title: '',
-        date: null,
-        file: null,
+        title: 'What day is today',
+        date: '2018-08-06',
+        file: 'test',
         Comment: '',
-      }],
-      active: 1,
-      date: null,
-      fileList: [{
-        name: 'food.jpeg',
-        url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100',
+        show: false,
       }, {
-        name: 'food2.jpeg',
-        url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100',
+        title: 'What day is today',
+        date: '2018-08-06',
+        file: 'test',
+        Comment: '',
+        show: false,
       }],
+      additem: {
+        title: '',
+        date: '',
+        file: '',
+        Comment: '',
+        show: false,
+      },
+      active: 1,
+
     };
   },
 
   methods: {
+    editView(index) {
+      console.log(this.Task);
+      this.Task.forEach((element) => {
+        const item = element;
+        item.show = false;
+      });
+      this.Task[index].show = true;
+    },
     change(i) {
       this.active = i;
     },
-    handleRemove(file, fileList) {
-      console.log(file, fileList);
+    getTask() {
+      return this.Task.sort(item => item.count > 6);
     },
-    handlePreview(file) {
-      console.log(file);
+    addTask(item) {
+      const newitem = item;
+      console.log(newitem);
+      newitem.show = false;
+      this.Task.push(item);
     },
-    handleExceed(files, fileList) {
-      this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
-    },
-    beforeRemove(file) {
-      return this.$confirm(`确定移除 ${file.name}？`);
+    showAddView(bool) {
+      this.additem.show = bool;
+      console.log(this.additem);
     },
   },
 };
@@ -85,18 +163,49 @@ export default {
 
 
 <style>
-.bg-gray{
-background: #E4E7ED;
-width: 2em;
-height: 2em;
-display: flex;
-justify-content: center;
-align-items: center;
+body{
+  background: #E1E1E1;
 }
+.View{
+  border-radius:3px;
+width: 800px;
+}
+  .title-text {
+    border-width: 0;
+    flex: 1;
+    font-size: 16pt;
+    background: transparent;
+  }
+
+  .bottomLine {
+    border-bottom: black solid 1px;
+  }
+
+  .addbox-font {
+    font-size: 16pt;
+    color: gray;
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .bg-gray {
+    background: #E4E7ED;
+    width: 2em;
+    height: 2em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
   .header {
     background: #4A90E2;
     color: #00408B;
     font-size: 20pt;
+    margin: 0 auto;
+    min-width: 700px;
   }
 
   .app {
@@ -105,12 +214,19 @@ align-items: center;
 
   .card {
     margin-top: 1em;
+    width: 800px;
     border: solid 1px #E1E1E1;
   }
 
   .active {
     color: #ffffff;
     border-bottom: solid 5px #00408B;
+  }
+
+  .addbox {
+    background: white;
+    border: 1px solid gray;
+    border-radius: 5px;
   }
 
   input[type="file"] {
@@ -122,6 +238,10 @@ align-items: center;
     overflow: hidden;
     clip: rect(0, 0, 0, 0);
     border: 0;
+  }
+
+  .View {
+    background: #F2F2F2;
   }
 
 </style>
